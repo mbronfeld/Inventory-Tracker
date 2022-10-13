0ec143b086f8469e85e2ea272bc950f959d16fe4
mbronfeld <79292851+mbronfeld@users.noreply.github.com>
2022-10-13 15:02:17 -0400

    Add files via upload

---
+++ tracker.py
@@ -0,0 +1,33 @@
+from time import sleep
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+import pandas as pd
+
+def main():
+    browser = webdriver.Chrome()
+    finalList = []
+    delay = 20 #seconds
+    browser.get("https://www.clover.com/dashboard/login")
+    try:
+        myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, "email-input")))
+    except TimeoutException:
+        print("Browser failed to load log-in page in time, check your internet connection")
+    emailSection = browser.find_element(By.ID, "email-input")
+    emailSection.send_keys("doda.midnight@thecorp.org")
+    passwordSection = browser.find_element(By.ID, "password-input")
+    passwordSection.send_keys("Jfk1961.")
+    logInButton = browser.find_element(By.ID, "log-in")
+    logInButton.send_keys()
+    logInButton.click()
+    try:
+        myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Reporting")))
+    except TimeoutException:
+        print("Browser failed to load main page time, check your internet connection")
+    reportingTab = browser.find_element(By.PARTIAL_LINK_TEXT, "Transactions")
+    reportingTab.click()
