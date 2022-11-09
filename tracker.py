ca9f509e417376dd4e21422fca9634afdd3a2e38
mbronfeld <msb305@georgetown.edu>
2022-11-09 10:27:41 -0500

    ;iasdfuha

--- tracker.py
+++ tracker.py
@@ -11,9 +11,7 @@ from datetime import date, timedelta, time
 #import matplotlib.pyplot as plt
 import numpy as np
 
-#Croissant Butter, Croissant Chocolate, Croissant Almond, Croissant Ham And Cheddar, Muffin Blueberry, Muffin Morning Glory, Cookie Chocolate Chip, Scone Blueberry, Coffee Cake, Cinnamon Bun, Scone Cheese and Cheddar, Scone Chocolate Chip, Bread Pumpkin, Pumpkin Cruffin
-#Croissant Butter, Croissant Chocolate, Croissant Almond, Croissant Ham And Cheddar, Muffin Blueberry, Muffin Morning Glory, Cookie Chocolate Chip, Scone Blueberry, Cinnamon Bun, Scone Cheese and Cheddar, Scone Chocolate Chip, Bread Pumpkin, Pumpkin Cruffin
-
+#Croissant Butter, Croissant Chocolate, Croissant Almond, Croissant Ham And Cheddar, Muffin Blueberry, Muffin Morning Glory, Cookie Chocolate Chip, Scone Blueberry, Scone Orange Cranberry, Scone Maple Walnut, Scone Chocolate Chip, Bread Pumpkin, Pumpkin Cruffin
 
 #TODO: plotting
 
